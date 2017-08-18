e9c716ce4c35eb8353e50fada8fc1bc77aa7079d
yang.yu01 <yang.yu01@istuary.com>
2017-08-18 16:31:05 +0800

    complate popup page

--- src/pages/modal/modal.ts
+++ src/pages/modal/modal.ts
@@ -26,5 +26,20 @@ export class ModalContentPage {
     console.log(this.amount);
     console.log(this.temperature);
     console.log(this.sweetness);
+    this.viewCtrl.dismiss();
+  }
+  select(item, type, mode) {
+    for( let col of item.path[2].children){
+      col.children[0].classList.remove("selected");
+    }
+    item.srcElement.classList.add("selected");
+    switch (type) {
+      case "temperature":
+        this.temperature = mode;
+        break;
+      default:
+        this.sweetness = mode;
+        break;
+    }
   }
 }
